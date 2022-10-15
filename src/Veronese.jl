a7c8d0714295831a78e2141b872be3aea476f9ce
Christopher <me@cgad.ski>
2022-10-15 10:09:46 -0300

    formatting, deleting old files

--- src/Veronese.jl
+++ src/Veronese.jl
@@ -5,103 +5,101 @@ module Veronese
 #
 include("util.jl")
 export random_coordinates,
-	random_vector, 
-	random_subspace,
-	complement_subspace,
-	annihilate_subspace,
-	coordinate_observation,
-	symmetric_product,
-	veronese,
-	un_veronese,
-	subspace_arrangement
+    random_vector,
+    random_subspace,
+    complement_subspace,
+    annihilate_subspace,
+    coordinate_observation,
+    symmetric_product,
+    veronese,
+    un_veronese,
+    subspace_arrangement
 
 #
 # Computing veronese rank of subspace arrangements.
 #
-export subspace_arrangement,
-	incidence_relations,
-	vhs_identifiable
+export subspace_arrangement, incidence_relations, vhs_identifiable
 
 function subspace_arrangement(n, k, d)
-	subspaces = zeros(n, d, k)
-	tensorized = zeros(n_multipairs(n), n_multipairs(d) * k)
+    subspaces = zeros(n, d, k)
+    tensorized = zeros(n_multipairs(n), n_multipairs(d) * k)
 
-	for i in 1:k
-		subspace = random_subspace(n, d)
-		subspaces[:, :, i] = subspace
-		tensorized[:, (1 + n_multipairs(d) * (i - 1)):(n_multipairs(d) * i)] = veronese(subspace)
-	end
-	
-	Vc = annihilate_subspace(tensorized)
-	V = annihilate_subspace(Vc)
-	return subspaces, V, Vc
+    for i = 1:k
+        subspace = random_subspace(n, d)
+        subspaces[:, :, i] = subspace
+        tensorized[:, (1+n_multipairs(d)*(i-1)):(n_multipairs(d)*i)] = veronese(subspace)
+    end
+
+    Vc = annihilate_subspace(tensorized)
+    V = annihilate_subspace(Vc)
+    return subspaces, V, Vc
 end
 
 function incidence_relations(W, Wc, V, Vc)
-	n, d = size(V)
-	l = size(W, 2)
-	nrels = n - d - l + 1
+    n, d = size(V)
+    l = size(W, 2)
+    nrels = n - d - l + 1
 
-	b = nullspace(Wc' * V, atol=1e-14)
-	a = nullspace(W' * Vc, atol=1e-14)
-	@assert size(b, 2) == 1
-	@assert size(a, 2) == nrels
+    b = nullspace(Wc' * V, atol = 1e-14)
+    a = nullspace(W' * Vc, atol = 1e-14)
+    @assert size(b, 2) == 1
+    @assert size(a, 2) == nrels
 
-	relations = zeros(d * (n - d), nrels)
-	for i in 1:(n - d - l + 1)
-		relations[:, i] = normalize(reshape(a[:, i] * b', d * (n - d)))
-	end
-	return relations
+    relations = zeros(d * (n - d), nrels)
+    for i = 1:(n-d-l+1)
+        relations[:, i] = normalize(reshape(a[:, i] * b', d * (n - d)))
+    end
+    return relations
 end
 
 function generate_summand(subspaces, V, Vc, r)
-	n, d, k = size(subspaces)
-	sample = subspaces[:, :, rand(1:k)] * random_subspace(d, 1)
-	incident = veronese(coordinate_observation(sample, random_coordinates(n, r)))
-	W, Wc = complement_subspace(incident)
-	relations = incidence_relations(W, Wc, V, Vc)
-	relations * relations'
+    n, d, k = size(subspaces)
+    sample = subspaces[:, :, rand(1:k)] * random_subspace(d, 1)
+    incident = veronese(coordinate_observation(sample, random_coordinates(n, r)))
+    W, Wc = complement_subspace(incident)
+    relations = incidence_relations(W, Wc, V, Vc)
+    relations * relations'
 end
 
-function vhs_identifiable(n, k, d, r; verbose=false)
-	if r <= d
-		return false
-	end
-	
-	subspaces, V, Vc = subspace_arrangement(n, k, d)
+function vhs_identifiable(n, k, d, r; verbose = false)
+    if r <= d
+        return false
+    end
 
-	N, tensorized_dim = size(V)
-	dof = tensorized_dim * (N - tensorized_dim)
+    subspaces, V, Vc = subspace_arrangement(n, k, d)
 
-	dof_per = N - tensorized_dim - binomial(n - r + 2, 2) + 1
-	if dof_per <= 0
-		return false
-	end
+    N, tensorized_dim = size(V)
+    dof = tensorized_dim * (N - tensorized_dim)
 
-	if verbose
-		println("truth has dimension $tensorized_dim in $N, $dof DOF")
-		println("expected DOF per sample: $dof_per")
-		println("expected samples: $(dof / dof_per)")
-	end
+    dof_per = N - tensorized_dim - binomial(n - r + 2, 2) + 1
+    if dof_per <= 0
+        return false
+    end
 
-	var = zeros(dof, dof)
+    if verbose
+        println("truth has dimension $tensorized_dim in $N, $dof DOF")
+        println("expected DOF per sample: $dof_per")
+        println("expected samples: $(dof / dof_per)")
+    end
 
-	i = 0
-	while i < ceil(dof * 5 / dof_per)
-		i += 1
-		var += generate_summand(subspaces, V, Vc, r)
+    var = zeros(dof, dof)
 
-		if mod(i, 10) == 0
-			c = cond(var)
-			if verbose
-				println("    $i: $c")
-			end
-			if c < 1e5
-				return true
-			end
-		end
-	end
-	return false
+    i = 0
+    while i < ceil(dof * 5 / dof_per)
+        i += 1
+        var += generate_summand(subspaces, V, Vc, r)
+
+        if mod(i, 10) == 0
+            c = cond(var)
+            if verbose
+                println("    $i: $c")
+            end
+            if c < 1e5
+                return true
+            end
+        end
+    end
+    return false
 end
 
 end
--- src/util.jl
+++ src/util.jl
@@ -1,96 +1,94 @@
 using LinearAlgebra
 using Random: randperm
 
-random_coordinates(n, k) = BitVector(
-	sum(Matrix(I, n, n)[:, randperm(n)[1:k]], dims = 2)[:, 1]
-	)
+random_coordinates(n, k) =
+    BitVector(sum(Matrix(I, n, n)[:, randperm(n)[1:k]], dims = 2)[:, 1])
 
-function random_vector(n::Int) 
-	v = randn(n)
-	(1 / norm(v)) * v
+function random_vector(n::Int)
+    v = randn(n)
+    (1 / norm(v)) * v
 end
 
 random_subspace(n::Int, d::Int) = qr(reshape(randn(n * d), n, d)).Q[:, 1:d]
 
 function complement_subspace(m)
-	fact = qr(m)
-	rank = sum(abs.(diag(fact.R)) .> 1e-15)
-	fact.Q[:, 1:rank], fact.Q[:, (rank + 1):end]
+    fact = qr(m)
+    rank = sum(abs.(diag(fact.R)) .> 1e-15)
+    fact.Q[:, 1:rank], fact.Q[:, (rank+1):end]
 end
 
 annihilate_subspace(m) = nullspace(transpose(m))
 
 "Incident subspace associated with a coordinate observation."
 function coordinate_observation(vector, observed)
-	n = length(vector)
-	k = n - sum(observed)
+    n = length(vector)
+    k = n - sum(observed)
 
-	incident::Matrix{Float64} = zeros((length(vector), k + 1))
-	begin
-		idx = 1
-		for i = 1:n
-			if !observed[i]
-				incident[i, idx] = 1
-				idx += 1
-			end
-		end
+    incident::Matrix{Float64} = zeros((length(vector), k + 1))
+    begin
+        idx = 1
+        for i = 1:n
+            if !observed[i]
+                incident[i, idx] = 1
+                idx += 1
+            end
+        end
 
-		incident[:, end] = normalize([!observed[i] ? 0 : vector[i] for i in 1:n])
-	end
+        incident[:, end] = normalize([!observed[i] ? 0 : vector[i] for i = 1:n])
+    end
 
-	return incident
+    return incident
 end
 
 n_multipairs(n::Integer) = binomial(n + 1, 2)
 
 function symmetric_product(v::Vector{Float64}, w::Vector{Float64})
-	n = length(v)
-	o = Vector{Float64}(undef, n_multipairs(n))
-	k = 1
-	for i in 1:n
-		for j in i:n
-			o[k] = i == j ? v[i] * w[i] : (v[i] * w[j] + v[j] * w[i])
-			k += 1
-		end
-	end
-	return o
+    n = length(v)
+    o = Vector{Float64}(undef, n_multipairs(n))
+    k = 1
+    for i = 1:n
+        for j = i:n
+            o[k] = i == j ? v[i] * w[i] : (v[i] * w[j] + v[j] * w[i])
+            k += 1
+        end
+    end
+    return o
 end
 
 veronese(v::Vector{Float64}) = symmetric_product(v, v)
 
 "The symmetric tensor power for vectors or linear maps."
 function veronese(m::Matrix{Float64})
-	n, d = size(m)
-	columns::Matrix{Float64} = zeros((n_multipairs(n), n_multipairs(d)))
-	k = 1
-	for i in 1:d
-		for j in i:d
-			columns[:, k] = symmetric_product(m[:, i], m[:, j])
-			k += 1
-		end
-	end
-	return columns
+    n, d = size(m)
+    columns::Matrix{Float64} = zeros((n_multipairs(n), n_multipairs(d)))
+    k = 1
+    for i = 1:d
+        for j = i:d
+            columns[:, k] = symmetric_product(m[:, i], m[:, j])
+            k += 1
+        end
+    end
+    return columns
 end
 
 function vector_to_symmetric(v::Vector{Float64}, n)
-	m::Matrix{Float64} = Matrix(undef, n, n)
-	k = 1
-	for i in 1:n
-		for j in i:n
-			if i == j
-				m[i, j] = v[k]
-			else
-				m[i, j] = m[j, i] = 0.5 * v[k]
-			end
-			k += 1
-		end
-	end
-	return Symmetric(m)
+    m::Matrix{Float64} = Matrix(undef, n, n)
+    k = 1
+    for i = 1:n
+        for j = i:n
+            if i == j
+                m[i, j] = v[k]
+            else
+                m[i, j] = m[j, i] = 0.5 * v[k]
+            end
+            k += 1
+        end
+    end
+    return Symmetric(m)
 end
 
 function un_veronese(v::Vector{Float64}, n::Int)
-	mat = vector_to_symmetric(v, n)
-	lambda, v = eigen(mat, n:n)
-	return sqrt(lambda[1]) * v[:, 1]
+    mat = vector_to_symmetric(v, n)
+    lambda, v = eigen(mat, n:n)
+    return sqrt(lambda[1]) * v[:, 1]
 end
-
