283534b71f5806731a7c846b5a33a49fc8e5bd48
Christopher <me@cgad.ski>
2022-11-09 16:44:06 -0300

    script for computing the vhs number

--- script.jl
+++ script.jl
@@ -1,14 +1,26 @@
 using Veronese
 
-# # adjust the parameters n, k, d here
-# arrangement_well_posed(21, 7, 3, 4)
+function vhs_number(k, d)
+    println("$k, $d")
+    vhs = d + 1
+    while true
+        print("   trying $vhs... ")
+        if vhs_identifiable(k * d, k, d, vhs; verbose=true)
+            println("  success")
+            return vhs
+        else
+            println("  failed")
+        end
+        vhs += 1
+    end 
+end
 
-n, k, d, r = 10, 3, 3, 5
-
-data, V, Vc = subspace_arrangement(n, k, d)
-N, tensorized_dim = size(V)
-dof = tensorized_dim * (N - tensorized_dim)
-var = zeros(dof, dof)
-sample = data[:, :, rand(1:k)] * random_subspace(d, 1)
-incident = veronese(coordinate_observation(sample, random_coordinates(n, r)))
-W, Wc = complement_subspace(incident)
+function ladmc_number(k, d)
+    ladmc = d + 1 
+    while true
+        if binomial(ladmc + 1, 2) > k * binomial(d + 1, 2)
+            return ladmc
+        end
+        ladmc += 1
+    end
+end
